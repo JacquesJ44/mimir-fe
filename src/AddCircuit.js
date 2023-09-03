fce3d997594ccca506c513dc04e01840cb7f0b5d
Jacques Du Toit <jacquesdutoit@Jacquess-MacBook-Air-2.local>
2023-09-04 00:39:29 +0200

    Circuits.js

--- src/AddCircuit.js
+++ src/AddCircuit.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 const AddCircuit = () => {
-     
+    
+    // const [value, setValue] = useState('')
+    
     const [vendor, setVendor] = useState('');
     const [circuitType, setCircuitType] = useState('');
     const [speed, setSpeed] = useState('');
@@ -14,6 +16,38 @@ const AddCircuit = () => {
     const [vlan, setVlan] = useState('');
     const [enni, setEnni] = useState('');
 
+    const vendors = [
+        {id: 0, label: "DFA", value: "DFA"},
+        {id: 1, label: "Seacom", value: "Seacom"},
+        {id: 2, label: "Comsol", value: "Comsol"},
+        {id: 3, label: "Frogfoot", value: "Frogfoot"},
+
+    ]
+
+    const circuitTypes = [
+        {id: 0, label: "Magellan", value: "Magellan"},
+        {id: 1, label: "Calypte", value: "Calypte"},
+        {id: 2, label: "GPON", value: "GPON"},
+        {id: 3, label: "CX Broadband", value: "CX Broadband"},
+    ]
+    
+    const speeds = [
+        {id: 0, label: "10Mbps", value: "10Mbps"},
+        {id: 1, label: "20Mbps", value: "20Mbps"},
+        {id: 2, label: "50Mbps", value: "50Mbps"},
+        {id: 3, label: "100Mbps", value: "100Mbps"},
+    ]
+
+    const contractTerms = [
+        {id: 0, label: "12 Months", value: "12 Months"},
+        {id: 1, label: "24 Months", value: "24 Months"},
+        {id: 2, label: "36 Months", value: "36 Months"},
+        {id: 3, label: "60 Months", value: "60 Months"},
+    ]
+
+    
+
+
 
     return ( 
         // <div className="h-screen flex items-center justify-center border">
@@ -24,42 +58,48 @@ const AddCircuit = () => {
                 {/* Row 1 */}
                 <div className="border flex">
                     <div className="form-control flex-auto">
-                        <label className="label">
-                            <span className="label-text">Vendor</span>    
+                        <label htmlFor="vendor" className="label">
+                            <span className="label-text">Vendor</span>
                         </label>
-                        <input className="input input-bordered w-full max-w-xs"
-                            type="text" 
-                            placeholder="Vendor"  
-                            required
-                            value = { vendor }
-                            onChange={(e) => setVendor(e.target.value)}
-                            />
+                        {/* <div>  */}
+                            <select onChange={(e) => setVendor(e.target.value)} id="vendor" className="input input-bordered w-full max-w-xs">
+                                    {vendors.map(vendormap => {
+                                        return (
+                                            <option key={vendormap.id} value={vendormap.value}>{vendormap.label}</option>
+                                        )
+                                    })}
+                            </select>
+                        {/* </div> */}
                     </div>
 
                     <div className="form-control flex-auto">
-                        <label className="label">
-                            <span className="label-text">Circuit Type</span>    
+                        <label htmlFor="circuittype" className="label">
+                            <span className="label-text">Circuit Type</span>
                         </label>
-                        <input className="input input-bordered w-full max-w-xs"
-                            type="text" 
-                            placeholder="Circuit Type"
-                            required
-                            value = { circuitType }
-                            onChange={(e) => setCircuitType(e.target.value)} 
-                        />
-                    </div>    
+                        {/* <div>  */}
+                            <select onChange={(e) => setCircuitType(e.target.value)} id="circuittype" className="input input-bordered w-full max-w-xs">
+                                    {circuitTypes.map(vendormap => {
+                                        return (
+                                            <option key={vendormap.id} value={vendormap.value}>{vendormap.label}</option>
+                                        )
+                                    })}
+                            </select>
+                        {/* </div> */}
+                    </div>
 
                     <div className="form-control flex-auto">
-                        <label className="label">
-                            <span className="label-text">Speed</span>    
+                        <label htmlFor="speed" className="label">
+                            <span className="label-text">Speed</span>
                         </label>
-                        <input className="input input-bordered w-full max-w-xs"
-                            type="text" 
-                            placeholder="Speed"
-                            required
-                            value = { speed }
-                            onChange={(e) => setSpeed(e.target.value)} 
-                            />
+                        {/* <div>  */}
+                            <select onChange={(e) => setSpeed(e.target.value)} id="speed" className="input input-bordered w-full max-w-xs">
+                                    {speeds.map(vendormap => {
+                                        return (
+                                            <option key={vendormap.id} value={vendormap.value}>{vendormap.label}</option>
+                                        )
+                                    })}
+                            </select>
+                        {/* </div> */}
                     </div>
 
                     <div className="form-control flex-auto">
@@ -120,16 +160,18 @@ const AddCircuit = () => {
                     </div>
 
                     <div className="form-control flex-auto">
-                        <label className="label">
-                            <span className="label-text">Contract Term</span>    
+                        <label htmlFor="contractterm" className="label">
+                            <span className="label-text">Contract Term</span>
                         </label>
-                        <input className="input input-bordered w-full max-w-xs"
-                            type="text" 
-                            placeholder="Contract Term"
-                            required
-                            value = { contractTerm }
-                            onChange={(e) => setContractTerm(e.target.value)} 
-                        />
+                        {/* <div>  */}
+                            <select onChange={(e) => setContractTerm(e.target.value)} id="contractterm" className="input input-bordered w-full max-w-xs">
+                                    {contractTerms.map(vendormap => {
+                                        return (
+                                            <option key={vendormap.id} value={vendormap.value}>{vendormap.label}</option>
+                                        )
+                                    })}
+                            </select>
+                        {/* </div> */}
                     </div>
 
                     <div className="form-control flex-auto">
@@ -179,6 +221,10 @@ const AddCircuit = () => {
                     <button className="btn btn-accent w-full max-w-xs">Save</button>
                 </div>
             </form>
+            <p>{vendor}</p>
+            <p>{circuitType}</p>
+            <p>{speed}</p>
+            <p>{contractTerm}</p>
         </div>
         // </div>
         // </div>
